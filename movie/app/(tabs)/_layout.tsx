12205dc97724c1dd02e5db6259274476762371c5
abhaythanak <abhaythanak@gmail.com>
2025-06-25 21:45:21 +0530

    custom hook build for fetch tmdb movie api and card created clickable with there id

--- movie/app/(tabs)/_layout.tsx
+++ movie/app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row items-center justify-center px-4 py-[14.1px] mt-4 rounded-full overflow-hidden transition-opacity duration-500"
+        className="flex flex-row items-center justify-center px-4 py-[14.1px] mt-4 rounded-full overflow-hidden "
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
